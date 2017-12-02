38876aae8b1d9fc896296bdba8fe294d49f6c017
Leandro Motta Barros <lmb@stackedboxes.org>
2017-12-02 09:16:53 -0200

    Initial Player node and script

---
+++ src/scenes/player.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+var speed = 200
+var velocity = Vector2()
+onready var screenSize = get_viewport_rect().size
+
+
+func _process(delta):
+	velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screenSize.x)
+	position.y = clamp(position.y, 0, screenSize.y)
+	
+	
