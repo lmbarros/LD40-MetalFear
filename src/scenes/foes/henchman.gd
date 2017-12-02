0dfc42743e88856ed281e25257d6ff340b51f67b
Leandro Motta Barros <lmb@stackedboxes.org>
2017-12-02 19:42:46 -0200

    Add some AI for attacking enemies

--- src/scenes/foes/henchman.gd
+++ src/scenes/foes/henchman.gd
@@ -1,2 +1,1 @@
 extends 'res://scripts/foe.gd'
-
--- src/scripts/foe.gd
+++ src/scripts/foe.gd
@@ -13,9 +13,13 @@ var patrolIndex = -1
 var patrolTargetPoint = null
 
 func _ready():
+	$attackModeTimer.connect("timeout", self, "changeAttackMode")
+	$shootTimer.connect("timeout", self, "shoot")
+	
 	initVision()
 	patrol = $patrol
 	nextPatrolPoint()
+	randomizeAttackMode()
 
 func nextPatrolPoint():
 	if patrol == null || patrol.get_child_count() == 0:
@@ -50,9 +54,6 @@ func _process(delta):
 	else:
 		doPatrol()
 
-func doAttack():
-	print("Attacking!")
-
 func doPatrol():
 	if patrol == null:
 		return
@@ -63,6 +64,104 @@ func doPatrol():
 	var velocity = (patrolTargetPoint - position).normalized() * speed * 0.5
 	move_and_slide(velocity)
 	rotation = velocity.angle() + PI/2
+
+# ------------------------------------------------------------------------------
+# Attack
+# ------------------------------------------------------------------------------
+export var changeAttackModeInterval = 7.0
+export var shootInterval = 0.5
+
+var randomTargetPoint = null
+
+enum {
+	attackModeWait,
+	attackModeShoot,
+	attackModeMoveTowardsPlayer,
+	attackModeMoveRandomly,
+}
+
+var attackMode = attackModeMoveTowardsPlayer
+
+func randomizeAttackMode():
+	var r = randf()
+	if r < 0.4: attackMode = attackModeShoot
+	elif r < 0.7: attackMode = attackModeMoveTowardsPlayer
+	elif r < 0.9: attackMode = attackModeMoveRandomly
+	else: attackMode = attackModeWait
+
+func doAttack():
+	if $attackModeTimer.is_stopped():
+		setAttackModeTimer()
+
+	match (attackMode):
+		attackModeWait:
+			doAttackWait()
+		attackModeShoot:
+			doAttackShoot()
+		attackModeMoveTowardsPlayer:
+			doAttackMoveTowardsPlayer()
+		attackModeMoveRandomly:
+			doAttackMoveRandomly()
+
+func doAttackWait():
+	rotation = (Player.position - position).angle() + PI/2
+
+func setShootTimer():
+	var delta = (randf() - 0.5) * shootInterval * 0.1
+	var timeForShooting = shootInterval + delta
+	timeForShooting = clamp(timeForShooting, 0.05, 3600.0)
+	$shootTimer.wait_time = timeForShooting
+	$shootTimer.start()
+	
+func setAttackModeTimer():
+	var delta = (randf() - 0.5) * changeAttackModeInterval * 0.1
+	var timeForChanging = changeAttackModeInterval + delta
+	timeForChanging = clamp(timeForChanging, 2.5, 3600.0)
+	$attackModeTimer.wait_time = timeForChanging
+	$attackModeTimer.start()
+	
+func changeAttackMode():
+	$shootTimer.stop()
+	randomizeAttackMode()
+	setAttackModeTimer()
+
+func doAttackShoot():
+	rotation = (Player.position - position).angle() + PI/2
+
+	if $shootTimer.is_stopped():
+		setShootTimer()
+
+func doAttackMoveTowardsPlayer():
+	var velocity = (Player.position - position).normalized() * speed
+	move_and_slide(velocity)
+	rotation = velocity.angle() + PI/2
+	
+func randomizeTargetPosition():
+	var screenSize = get_viewport_rect().size
+	randomTargetPoint = Vector2(
+		int(rand_range(0, screenSize.x)),
+		int(rand_range(0, screenSize.y)))
+
+	
+func doAttackMoveRandomly():
+	if randomTargetPoint == null:
+		randomizeTargetPosition()
+
+	if position.distance_to(randomTargetPoint) < patrolArrivalDistance:
+		randomizeTargetPosition()
+
+	var velocity = (randomTargetPoint - position).normalized() * speed
+	move_and_slide(velocity)
+	rotation = velocity.angle() + PI/2
+
+
+func shoot():
+	var aim = (randf() - 0.5) * 0.15
+	rotation = (Player.position - position).angle() + PI/2 + aim
+
+	print("BANG!")
+	setShootTimer()
+
 
 # ------------------------------------------------------------------------------
 # Vision
