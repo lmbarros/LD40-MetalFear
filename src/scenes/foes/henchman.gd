f82735b3a3b05017b1db9d3ff575b07854604ca9
Leandro Motta Barros <lmb@stackedboxes.org>
2017-12-02 14:37:32 -0200

    Bugfix: dying henchmen keep dying when under fire

--- src/scenes/foes/henchman.gd
+++ src/scenes/foes/henchman.gd
@@ -2,14 +2,19 @@ extends Area2D
 
 export var health = 50.0
 
+var isDying = false
+
 func hit(weapon):
+	if isDying:
+		return
+
 	health -= weapon.damage
-	print (health)
-	if health > 0.0:
-		$anim.play("hit")
-	else:
+	if health < 0.0:
+		isDying = true
 		$anim.play("death")
 		print("Augh!")
 		yield($anim, "animation_finished")
 		print("Removing")
 		queue_free()
+	else:
+		$anim.play("hit")
