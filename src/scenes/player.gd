ed5e88273264adca7e50885fc8932006a56a239b
Leandro Motta Barros <lmb@stackedboxes.org>
2017-12-02 11:51:18 -0200

    Can switch weapons

--- src/scenes/player.gd
+++ src/scenes/player.gd
@@ -4,12 +4,35 @@ var speed = 200
 var velocity = Vector2()
 onready var screenSize = get_viewport_rect().size
 
+var weapons = [ Weapons.fist, Weapons.pistol ]
+var weapon = 0
+
 signal hitWithGun
+signal hitWithMelee
 
 func _ready():
 	# Don't shoot yourself (in the foot or otherwise)
 	# (Why doesn't "ignore parent" work here?)
 	$shotRayCast.add_exception(Player)
+
+func nextWeapon():
+	weapon += 1
+	if weapon >= weapons.size():
+		weapon = 0
+	updateWeapon()
+
+func prevWeapon():
+	weapon -= 1
+	if weapon < 0:
+		weapon = weapons.size() - 1
+	updateWeapon()
+
+func updateWeapon():
+	match weapons[weapon]:
+		Weapons.fist:
+			$sprite/weapon.animation = "fistIdle"
+		Weapons.pistol:
+			$sprite/weapon.animation = "pistolIdle"
 
 func _process(delta):
 	# Walk
@@ -32,6 +55,12 @@ func _process(delta):
 	if velocity.length_squared() > 0:
 		rotation = velocity.angle() + PI/2
 	
+	# Switch weapons
+	if Input.is_action_just_pressed("next_weapon"):
+		nextWeapon()
+	elif Input.is_action_just_pressed("prev_weapon"):
+		prevWeapon()
+
 	# Shoot
 	if Input.is_action_just_pressed("shoot"):
 		$shotRayCast.force_raycast_update()
---
+++ src/scripts/weapons.gd
@@ -0,0 +1,17 @@
+extends Node
+
+const melee = "melee"
+const firearm = "firearm"
+
+
+var fist = {
+	type = melee,
+	damage = 2.0,
+	metal = 0.0,
+}
+
+var pistol = {
+	type = firearm,
+	damage = 15.0,
+	metal = 1.1,
+}
