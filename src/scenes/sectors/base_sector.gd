9ac044d9a7f9c36c914ab05fcea13d09d90cedd8
Leandro Motta Barros <lmb@stackedboxes.org>
2017-12-03 15:25:17 -0200

    Spawn foes when global alarm rings

--- src/scenes/sectors/base_sector.gd
+++ src/scenes/sectors/base_sector.gd
@@ -26,5 +26,12 @@ func sectorPath(sector):
 # ------------------------------------------------------------------------------
 # Alarm-related stuff
 # ------------------------------------------------------------------------------
+onready var Henchman = preload("res://scenes/foes/henchman.tscn")
+
 func globalAlarmRung():
-	print("uuuuuuuuuuuuuuuummm!!!!")
+	# Spawn foes
+	for i in range($spawnPoints.get_child_count()):
+		var pos = $spawnPoints.get_child(i).position
+		var foe = Henchman.instance()
+		foe.position = pos
+		get_tree().get_current_scene().add_child(foe)
